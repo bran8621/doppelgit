4a8d2f95e1497cf66987e22db181f40c959ebe3e
bran8621 <bran8621@gmail.com>
2024-04-08 20:53:49 -0400

    Ammended get_object and hash_object, implemented write_tree, cat_file, created base.py

--- doppelgit/cli.py
+++ doppelgit/cli.py
@@ -1,7 +1,8 @@
 import argparse
-import hashlib
+import sys
 import os
-from . import data
+import data
+import base
 
 def main():
     args = parse_args()
@@ -21,7 +22,14 @@ def parse_args():
 
     hash_object_parser = commands.add_parser('hash-object')
     hash_object_parser.add_argument('file', type = str)
-    hash_object_parser.set_defaults(func = hash_object)
+    hash_object_parser.set_defaults(func = data.hash_object)
+
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.add_argument('object', type = str)
+    cat_file_parser.set_defaults(func = data.cat_file)
+
+    write_tree_parser = commands.add_parser('write-tree')
+    write_tree_parser.set_defaults(func = base.write_tree)
 
     return parser.parse_args()
 
@@ -44,5 +52,9 @@ def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
 
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object, expected = None))
 
-    
+def write_tree(args):
+    base.write_tree(args)
--- doppelgit/data.py
+++ doppelgit/data.py
@@ -8,14 +8,22 @@ def init():
     os.makedirs(GIT_DIR)
     os.makedirs(f'{GIT_DIR}/objects')
 
-def hash_object(data):
-
-    oid = hashlib.sha1(data).hexdigest()
+def hash_object(data, type = 'blob'):
+    object = type.encode() + b'\x00' + data
+    oid = hashlib.sha1(object).hexdigest()
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as f:
-        f.write(data)
-
+        f.write(object)
     return oid
 
-def get_object(oid):
+def get_object(oid, expected = 'blob'):
     with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
-        return f.read()
+        object = f.read()
+    
+    type_, _, content = object.partition(b'\x00')
+    type_ = type_.decode()
+
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected} but got {type_}'
+    return content
+
+
